284062d55a8ed10375a3b721df3112215d0e682f
ligy118 <liguanya118@gmail.com>
2016-05-17 21:21:45 +0000

    .

--- 5_11/gaojingdu.cpp
+++ 5_11/gaojingdu.cpp
@@ -1,8 +1,9 @@
-#include<iostream>
 #include<stdio.h>
 #include<string.h>
-using namespace std;
-void cinnn(int *,int *);
+//#include<iostream>
+//using namespace std;
+void cinnn(int *,int *); //输入函数
+void coutt(int *); //输出函数
 void add(int *,int *,int *);
 int main()
 {
@@ -11,10 +12,10 @@ int main()
     int ans[35];
     for(i=0;i<35;i++) left[i]=right[i]=0;
     cinnn(left,right);
-    for(i=0;i<30;i++) cout<<left[i];
-    cout<<endl;
-    for(i=0;i<30;i++) cout<<right[i];
-    cout<<endl;
+    coutt(left);
+    printf("\n");
+    coutt(right);
+    printf("\n");
     return 0;
 }
 void cinnn(int left[],int right[])
@@ -35,13 +36,26 @@ void cinnn(int left[],int right[])
     else bpoint=bn;
     //cout<<an<<"   "<<bn<<endl;
     //cout<<apoint<<"   "<<bpoint<<endl;
-    for(i=20-apoint+1;i<=20;i++) left[i]=a[i-21+apoint]-'0';
-    for(i=21;i<21+an-apoint-1;i++) left[i]=a[i-21+1+apoint]-'0';
-    for(i=20-bpoint+1;i<=20;i++) right[i]=b[i-21+bpoint]-'0';
-    for(i=21;i<21+bn-bpoint-1;i++) right[i]=b[i-21+1+bpoint]-'0';
+    for(i=19-apoint+1;i<=19;i++) left[i]=a[i-20+apoint]-'0';
+    for(i=20;i<20+an-apoint-1;i++) left[i]=a[i-20+1+apoint]-'0';
+    for(i=19-bpoint+1;i<=19;i++) right[i]=b[i-20+bpoint]-'0';
+    for(i=20;i<20+bn-bpoint-1;i++) right[i]=b[i-20+1+bpoint]-'0';
+}
+void coutt (int a[])
+{
+    int st,en;
+    int i;
+    for(i=0;i<30;i++) if(a[i]!=0) {st=i;break;}
+    for(i=29;i>=0;i--) if(a[i]!=0) {en=i;break;}
+    for(i=st;i<20&&i<=en;i++) printf("%d",a[i]);
+    if(en>=20)
+    {
+        printf(".");
+        for(i=20;i<30&&i<=en;i++) printf("%d",a[i]);
+    }
+
 }
 void add(int a[],int b[],int ans[])
 {
-
-;
+    ;
 }
