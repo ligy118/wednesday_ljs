7d5f44a127e5c3fcd4df49cc84344c480bd1b414
ligy118 <liguanya118@gmail.com>
2016-05-18 12:32:29 +0000

    可以交作业了23333

--- 5_11/gaojingdu.cpp
+++ 5_11/gaojingdu.cpp
@@ -1,25 +1,25 @@
 //不给数据规模，差评！
 //就当模拟做了
 //乘法有效范围极其有限
-//除法，那就呵呵了，困了，为了加减乘除完整应付的，且效率特别低，商越大，时间越长
+//除法，因为用到了乘法，所以有效范围也有限
 #include<stdio.h>
 #include<string.h>
 //#include<iostream>
 //using namespace std;
 void cinnn(int *,int *); //输入函数
 void coutt(int *); //输出函数
-int com(int *,int *); // return 0 left大,1 right 大
-void add(int *,int *,int *);
-void sub(int *,int *,int *);
-void mul(int *,int *,int *);
-void div(int *,int *,int *);
+int com(int *,int *); // 比较函数 return 0 left大,1 right 大
+void add(int *,int *,int *);// +
+void sub(int *,int *,int *);// -
+void mul(int *,int *,int *);// *
+void div(int *,int *,int *);// /
 int main()
 {
     int i,j,k;
     int left[100]={0},right[100]={0};
     int addans[100]={0},subans[100]={0},mulans[100]={0},divans[100]={0};
     cinnn(left,right);
-  /*  coutt(left);
+    /*coutt(left);
     printf("\n");
     coutt(right);
     printf("\n");*/
@@ -48,7 +48,7 @@ void cinnn(int left[],int right[])
     int an,bn,apoint,bpoint;
     int i,j,k;
     scanf("%s%s",a,b);
- //   cout<<a<<endl<<b<<endl;
+//  cout<<a<<endl<<b<<endl;
     an=strlen(a);
     bn=strlen(b);
     tapoint=strchr(a,'.');
@@ -57,8 +57,8 @@ void cinnn(int left[],int right[])
     else apoint=an;
     if(tbpoint) bpoint=tbpoint-b;
     else bpoint=bn;
-    //cout<<an<<"   "<<bn<<endl;
-    //cout<<apoint<<"   "<<bpoint<<endl;
+//  cout<<an<<"   "<<bn<<endl;
+//  cout<<apoint<<"   "<<bpoint<<endl;
     for(i=19-apoint+1;i<=19;i++) left[i]=a[i-20+apoint]-'0';
     for(i=20;i<20+an-apoint-1;i++) left[i]=a[i-20+1+apoint]-'0';
     for(i=19-bpoint+1;i<=19;i++) right[i]=b[i-20+bpoint]-'0';
@@ -68,9 +68,11 @@ void coutt (int a[])
 {
     int st,en;
     int i;
+    if(a[35]) printf("-");
     for(i=0;i<30;i++) if(a[i]!=0) {st=i;break;}
     for(i=29;i>=0;i--) if(a[i]!=0) {en=i;break;}
     for(i=st;i<20;i++) printf("%d",a[i]);
+    if(st>=20) printf("0");
     if(en>=20)
     {
         printf(".");
@@ -85,6 +87,7 @@ int com(int a[],int b[])
         if(a[i]>b[i]) return 0;
         else if(b[i]>a[i]) return 1;
     }
+    return 0;
 }
 void add(int a[],int b[],int ans[])
 {
@@ -103,13 +106,14 @@ void sub(int a[],int b[],int ans[])
     int i,j,k;
     if(com(a,b))
     {
+        ans[35]=1;
         sub(b,a,ans);
         return ;
     }
     for(i=29;i>=0;i--)
     {
         ans[i]=jinwei+a[i];
-     //    cout<<"! "<<jinwei<<"  "<<a[i]<<" !"<<endl;
+//        cout<<"! "<<jinwei<<"  "<<a[i]<<" !"<<endl;
         if(ans[i]<b[i])
         {
             ans[i]+=10;
@@ -146,20 +150,32 @@ void mul(int a[],int b[],int ans[])
 }
 void div(int a[],int b[],int ans[])
 {
-    int i;
-    int ttt[35]={0};
+    int i,j,k;
+    int vvv[28][35]={0};
     int chengji[35]={0};
-    ttt[22]=1;
-    for(;;)
+    int aans[35]={0};
+    for(i=1;i<28;i++) vvv[i][i]=1;
+
+    for(i=1;i<28;i++)
     {
-        for(i=0;i<30;i++) chengji[i]=0;
-        add(ttt,ans,ans);
-        mul(ans,b,chengji);
-    /*    cout<<"ans "; coutt(ans);cout<<"   ";
-        coutt(a);
-        cout<<"???"<<endl;
-        coutt(chengji);
-        cout<<endl;*/
-        if(com(a,chengji)) return ;
+        for(;;)
+        {
+            add(aans,vvv[i],aans);
+            for(j=0;j<30;j++) chengji[j]=0;
+            mul(aans,b,chengji);
+//        cout<<i<<"  aans: ";coutt(aans);cout<<endl;
+//            cout<<"chengji: ";coutt(chengji);cout<<endl;
+
+            if(!com(a,chengji))
+            {
+                for(k=0;k<30;k++) ans[k]=aans[k];
+
+            }
+            else
+            {
+                sub(aans,vvv[i],aans);
+                break;
+            }
+        }
     }
 }
